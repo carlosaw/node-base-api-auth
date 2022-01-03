747c12068c722a36f627f244b919c1959881cc5a
unknown <carlosfreevg@gmail.com>
2022-01-03 09:28:49 -0400

    Verify-OK Base64

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,18 +1,35 @@
 import { Request, Response, NextFunction } from 'express';
 import JWT from 'jsonwebtoken';
 import dotenv from 'dotenv';
+import { User } from '../models/User';
 
 dotenv.config();
 
 export const Auth = {
-  private: (req: Request, res: Response, next: NextFunction) => {
+  private: async (req: Request, res: Response, next: NextFunction) => {
     
     let success = false;// Não autorizado
     
     // Fazer verificação de auth
     if(req.headers.authorization) {
+      // Basic64
+      let hash: string = req.headers.authorization.substring(6);
+      let decoded: string = Buffer.from(hash, 'base64').toString();
+      let data: string[] = decoded.split(':');
+      if(data.length === 2) {
+        let hasUser = await User.findOne({
+          where: {
+            email: data[0],
+            password: data[1]
+          }
+        });
+        if(hasUser) {
+          success = true;
+        }
+      }
 
-      const [authType, token] = req.headers.authorization.split(' ');
+
+      /*const [authType, token] = req.headers.authorization.split(' ');
       if(authType === 'Bearer') {
         //console.log('TOKEN', token);
         try {
@@ -26,8 +43,9 @@ export const Auth = {
         } catch(err) {
           // Não mostra erro nenhum
         }
-      }
+      }*/
     }
+    
 
     if(success) {// Se é Sucesso
       next();// Passa para o próximo passo.
